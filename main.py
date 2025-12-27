f49b0bc7e7f2fff2c61b44a6893fcd0c445411e4
Casper3354 <denisganiev3354@gmail.com>
2025-12-27 17:56:55 +0300

    new branch

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def greet():
-    print("Hello world!")
+# def greet():
+#     print("Hello world!")
     
-if __name__ == "__main__":
-    greet()
+# if __name__ == "__main__":
+#     greet()
+
+def greet(name):
+    print(f"Hello, {name}!")
+    
+if __name__ == "__name__":
+    greet("Vasilisa")
